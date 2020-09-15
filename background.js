5cdc3224bacd14e03b69f368dd353701c4948a5c
jbrownrs <36203116+jbrownrs@users.noreply.github.com>
2020-09-15 16:36:44 +0100

    Removes reload script and tidies background and manifest

--- background.js
+++ background.js
@@ -6,13 +6,5 @@ window.browser = (function () {
 })();
 
 browser.browserAction.onClicked.addListener(function(tab) {
-    browser.storage.sync.get('state', function(data) {
-        if (data.state === 'on') {
-            browser.storage.sync.set({state: 'off'});
-            browser.tabs.executeScript(null, {file: "reload.js"});
-        } else {
-            browser.storage.sync.set({state: 'on'});
-            browser.tabs.executeScript(null, {file: "imageSwap.js"});
-        }
-    });
+    browser.tabs.executeScript(null, {file: "imageSwap.js"});
 });
--- reload.js
+++
@@ -1,6 +0,0 @@
-reloadTab();
-
-function reloadTab() {
-    location.reload();
-    // TODO: change this into reloading just the images
-}
